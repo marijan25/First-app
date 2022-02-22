8d19519b866a8daca8aa48f324333ea41d157c13
Marijan Ikanovic <marijan.ikanovic@symphony.is>
2022-02-22 16:16:56 +0100

    I modified my application using MUI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React from 'react';
-import Header from './components/Header'
-import Form from './components/Form';
-
+import "./App.css"
+import TourCard from './components/TourCard';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import AppBar from './components/AppBar';
+import cities from "./data.json"
+import {Typography} from "@mui/material";
 
 function App() {
   return (
     <div>
-      <Header title='My first app'/>
-      <Form />
+      <AppBar />
+      <Container sx={{ marginY: 5 }}>
+      {cities.map((city) => (
+        <>
+        <Typography 
+          variant = "h4"
+          component = "h2"
+          marginTop = {5}
+          marginBottom = {3}
+        >
+          Top {city.name} Tours
+        </Typography>
+        {<Grid container spacing={5}>
+          {city.tours.map((tour, index) => <TourCard tour = {tour} key = {index} /> )}
+        </Grid>
+      }
+        </>
+      ))}  
+     
+      </Container>
     </div>
   );
 }
